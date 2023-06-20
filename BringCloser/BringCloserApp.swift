dd9a1ba3f34bbba24e5702fcad0bef3828ae61e2
Viet Tung Le <viettung.le.2906@gmail.com>
2023-06-20 20:04:23 +0700

    Create project and add gitignore file

---
+++ BringCloser/BringCloserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BringCloserApp.swift
+//  BringCloser
+//
+//  Created by Le Viet Tung on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BringCloserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
